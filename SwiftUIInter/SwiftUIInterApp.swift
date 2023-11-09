b89d5a4abc967356e1a35e9c7b92f4c5b809cff4
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-11-09 17:12:47 +0100

    Initial Commit

---
+++ SwiftUIInter/SwiftUIInterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInterApp.swift
+//  SwiftUIInter
+//
+//  Created by Dmytro Nimchynskyi on 09/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
