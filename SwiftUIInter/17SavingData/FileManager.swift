eedc921cb39f19ab82939db78c5a84220e257459
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-11-25 21:15:12 +0100

    add saveImage & getImage

--- SwiftUIInter/17SavingData/FileManager.swift
+++ SwiftUIInter/17SavingData/FileManager.swift
@@ -7,12 +7,111 @@
 
 import SwiftUI
 
-struct FileManager: View {
+class LocalFileManager {
+    
+    static let instance = LocalFileManager()
+    
+    func saveImage(image: UIImage, name: String) {
+        
+        guard 
+            let data = image.jpegData(compressionQuality: 1.0),
+            let path = getPathForImage(name: name) else {
+            print("Error getting data.")
+            return
+        }
+        
+        do {
+            try data.write(to: path)
+            print("Success saving!")
+        } catch let error {
+            print("Error saving. \(error)")
+        }
+    }
+    
+    func getImage(name: String) -> UIImage? {
+        guard
+            let path = getPathForImage(name: name)?.path,
+            FileManager.default.fileExists(atPath: path) else {
+            print("Error getting path.")
+            return nil
+        }
+        
+        return UIImage(contentsOfFile: path)
+    }
+    
+    func getPathForImage(name: String) -> URL? {
+        guard
+            let path = FileManager
+                .default
+                .urls(for: .cachesDirectory, in: .userDomainMask)
+                .first?
+                .appending(path: "\(name).jpg") else {
+            print("Error getting path.")
+            return nil
+        }
+        
+        return path
+    }
+}
+
+class FileManagerViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    let imageName: String = "iphone-xr-in-hand"
+    let manager = LocalFileManager.instance
+    
+    init() {
+        getImageFromAssetsFolder()
+//        getImageFromFileManager()
+    }
+    
+    func getImageFromAssetsFolder() {
+        image = UIImage(named: imageName)
+    }
+    
+    func getImageFromFileManager() {
+        image = manager.getImage(name: imageName)
+    }
+    
+    func saveImage() {
+        guard let image = image else { return }
+        manager.saveImage(image: image, name: imageName)
+    }
+}
+
+struct FileManagerView: View {
+    
+    @StateObject var vm = FileManagerViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                
+                if let image = vm.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 200, height: 200)
+                        .clipped()
+                        .clipShape(.rect(cornerRadius: 10))
+                }
+                
+                Button {
+                    vm.saveImage()
+                } label: {
+                    Text("Save to FileManager")
+                        .font(.headline)
+                        .padding()
+                }
+                .buttonStyle(.borderedProminent)
+
+                Spacer()
+            }
+            .navigationTitle("File Manager")
+        }
     }
 }
 
 #Preview {
-    FileManager()
+    FileManagerView()
 }
