f60db97885761a7f9b661eb573ad4e322e7ce9af
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-11-26 17:57:01 +0100

    add caching view

---
+++ SwiftUIInter/17SavingData/Caching.swift
@@ -0,0 +1,18 @@
+//
+//  Caching.swift
+//  SwiftUIInter
+//
+//  Created by Dmytro Nimchynskyi on 26/11/2023.
+//
+
+import SwiftUI
+
+struct Caching: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Caching()
+}
