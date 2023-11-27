b4545f34436d2228a9b2cfdd863c41aa110161bc
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-11-27 16:24:02 +0100

    add photo download service

--- SwiftUIInter/17SavingData/DownloadAndCache/Utilities/PhotoModelDataService.swift
+++ SwiftUIInter/17SavingData/DownloadAndCache/Utilities/PhotoModelDataService.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import Combine
+
+class PhotoModelDataService {
+    static let instance = PhotoModelDataService() // Singleton
+    
+    @Published var photoModels: [PhotoModel] = []
+    var cancellables = Set<AnyCancellable>()
+    
+    private init() {
+        downloadData()
+    } // to have only one insatance
+    
+    func downloadData() {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/photos") else { return }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .tryMap(handleOutput)
+            .decode(type: [PhotoModel].self, decoder: JSONDecoder())
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("Error downloading data. \(error)")
+                }
+            } receiveValue: { [weak self] returnedPhotoModels in
+                self?.photoModels = returnedPhotoModels
+            }
+            .store(in: &cancellables)
+
+    }
+    
+    private func handleOutput(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard
+            let response = output.response as? HTTPURLResponse,
+            response.statusCode >= 200 && response.statusCode < 300 else {
+            throw URLError(.badServerResponse)
+        }
+        return output.data
+    }
+}
