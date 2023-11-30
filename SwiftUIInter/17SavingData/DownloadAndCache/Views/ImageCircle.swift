ff41c0a1feddb4359d96b5b9e1c7c70b4181cc72
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-11-30 21:35:21 +0100

    add separate image circle view

---
+++ SwiftUIInter/17SavingData/DownloadAndCache/Views/ImageCircle.swift
@@ -0,0 +1,37 @@
+//
+//  ImageCircle.swift
+//  SwiftUIInter
+//
+//  Created by Dmytro Nimchynskyi on 30/11/2023.
+//
+
+import SwiftUI
+
+struct ImageCircle: View {
+    
+    @StateObject var loader: ImageLoadingViewModel
+    
+    init(url: String) {
+        // !!! init of state object _
+        _loader =  StateObject(wrappedValue: ImageLoadingViewModel(url: url))
+    }
+    
+    
+    var body: some View {
+        ZStack {
+            if loader.isLoading {
+                ProgressView()
+            } else if let image = loader.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .clipShape(.circle)
+            }
+        }
+    }
+}
+
+#Preview {
+    ImageCircle(url: "https://via.placeholder.com/600/92c952")
+        .frame(width: 75, height: 75)
+        .previewLayout(.sizeThatFits)
+}
