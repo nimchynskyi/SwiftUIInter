9cd4bf21d711dfeb686353c42d5a220d3584b5e2
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-11-30 21:35:58 +0100

    update main image view model

--- SwiftUIInter/17SavingData/DownloadAndCache/ViewModels/DownloadImageViewModel.swift
+++ SwiftUIInter/17SavingData/DownloadAndCache/ViewModels/DownloadImageViewModel.swift
@@ -6,11 +6,28 @@
 //
 
 import Foundation
+import Combine
 
 class DownloadImageViewModel: ObservableObject {
     
     @Published var dataArray: [PhotoModel] = []
+    // can ref dataService and put in its cancellables but much better to separate
+    var cancellables = Set<AnyCancellable>()
     
     let dataService = PhotoModelDataService.instance
     
+    init() {
+        addSubscribers()
+    }
+    
+    // bc photoModels array is a publisher - i can subscribe to it to cont get publish values
+    
+    func addSubscribers() {
+        // $ is important here bc its a publisher
+        dataService.$photoModels
+            .sink { [weak self] returnedPhotoModels in
+                self?.dataArray = returnedPhotoModels
+            }
+            .store(in: &cancellables)
+    }
 }
