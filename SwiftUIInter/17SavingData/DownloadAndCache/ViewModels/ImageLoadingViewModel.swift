43531f16f76746723dafc9d86e6eb766bfadcefe
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-11-30 21:34:35 +0100

    update image model vm

--- SwiftUIInter/17SavingData/DownloadAndCache/ViewModels/ImageLoadingViewModel.swift
+++ SwiftUIInter/17SavingData/DownloadAndCache/ViewModels/ImageLoadingViewModel.swift
@@ -6,9 +6,43 @@
 //
 
 import Foundation
+import SwiftUI
 
 class ImageLoadingViewModel: ObservableObject {
     
     @Published var image: UIImage? = nil
     @Published var isLoading: Bool = false
+    
+    let urlString: String
+    
+    init(url: String) {
+        urlString = url
+        downloadImage()
+    }
+    
+    func downloadImage() {
+        isLoading = true
+        guard let url = URL(string: urlString) else {
+            isLoading = false
+            return
+        }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            /* same code as below
+            .map { data, response -> UIImage? in
+                return UIImage(data: data)
+            }
+            */
+            .map {
+                UIImage(data: $0.data)
+            }
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.isLoading = false
+            } receiveValue: { [weak self] returnedImage in
+                self?.image = returnedImage
+            }
+
+            
+    }
 }
