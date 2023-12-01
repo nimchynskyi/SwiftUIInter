7655b2013edf787c3f528d414c7564a0b76945f9
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-12-01 14:47:54 +0100

    update cache file manager

--- SwiftUIInter/17SavingData/DownloadAndCacheMiniApp/Utilities/PhotoModelFileManager.swift
+++ SwiftUIInter/17SavingData/DownloadAndCacheMiniApp/Utilities/PhotoModelFileManager.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+import SwiftUI
+
+class PhotoModelFileManager {
+    
+    static let instance = PhotoModelFileManager()
+    let folderName = "downloadedPhotos"
+    
+    private init() {
+        createFolderIfNeeded()
+    }
+    
+    func createFolderIfNeeded() {
+        guard let url = getFolderPath() else { return }
+        
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
+                print("Created folder!")
+            } catch let error {
+                print("Error crating folder. \(error)")
+            }
+        }
+    }
+    
+    private func getFolderPath() -> URL? {
+        return FileManager
+            .default
+            .urls(for: .cachesDirectory, in: .userDomainMask)
+            .first?
+            .appendingPathComponent(folderName)
+    }
+    
+    // .../downloadedPhotos/
+    // .../downloadedPhotos/image-name.png
+    private func getImagePath(key: String) -> URL? {
+        guard let folder = getFolderPath() else {
+            return nil
+        }
+        return folder.appendingPathComponent(key + ".png")
+    }
+    
+    func add(key: String, value: UIImage) {
+        guard
+            let data = value.pngData(),
+            let url = getImagePath(key: key) else { return }
+        
+        do {
+            try data.write(to: url)
+        } catch let error {
+            print("Error saving to path manager. \(error)")
+        }
+    }
+    
+    func get(key: String) -> UIImage? {
+        guard
+            let url = getImagePath(key: key),
+            FileManager.default.fileExists(atPath: url.path) else {
+            return nil
+        }
+        return UIImage(contentsOfFile: url.path)
+    }
+}
