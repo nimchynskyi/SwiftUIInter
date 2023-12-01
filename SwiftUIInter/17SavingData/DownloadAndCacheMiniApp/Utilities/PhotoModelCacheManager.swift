8108d7e800f230452950f0edf66b0fa7de1222ce
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-12-01 14:47:25 +0100

    update cache manager

--- SwiftUIInter/17SavingData/DownloadAndCacheMiniApp/Utilities/PhotoModelCacheManager.swift
+++ SwiftUIInter/17SavingData/DownloadAndCacheMiniApp/Utilities/PhotoModelCacheManager.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import SwiftUI
+
+class PhotoModelCacheManager {
+    
+    static let instance = PhotoModelCacheManager()
+    private init() {}
+    
+    var photoCache: NSCache<NSString, UIImage> = {
+        var cache = NSCache<NSString, UIImage>()
+        cache.countLimit = 200
+        cache.totalCostLimit = 1024 * 1024 * 200 // 200mb
+        return cache
+    }()
+    
+    func add(key: String, value: UIImage) {
+        photoCache.setObject(value, forKey: key as NSString)
+    }
+    
+    func get(key: String) -> UIImage? {
+        return photoCache.object(forKey: key as NSString)
+    }
+}
