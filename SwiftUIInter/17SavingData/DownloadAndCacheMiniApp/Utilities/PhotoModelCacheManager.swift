8b8fb6fae5d22dec8c086598c501d21d9cfebaeb
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-12-01 14:47:09 +0100

    add cache managers

---
+++ SwiftUIInter/17SavingData/DownloadAndCacheMiniApp/Utilities/PhotoModelCacheManager.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoModelCacheManager.swift
+//  SwiftUIInter
+//
+//  Created by Dmytro Nimchynskyi on 01/12/2023.
+//
+
+import Foundation
---
+++ SwiftUIInter/17SavingData/DownloadAndCacheMiniApp/Utilities/PhotoModelFileManager.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoModelFileManager.swift
+//  SwiftUIInter
+//
+//  Created by Dmytro Nimchynskyi on 01/12/2023.
+//
+
+import Foundation
