2636accefb89b9ab77d207d796d448bbac81a391
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-12-05 13:40:19 +0100

    refactor

---
+++ SwiftUIInter/11Accessibility/AccessibilityText.swift
@@ -0,0 +1,18 @@
+//
+//  AccessibilityText.swift
+//  SwiftUIInter
+//
+//  Created by Dmytro Nimchynskyi on 01/12/2023.
+//
+
+import SwiftUI
+
+struct AccessibilityText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccessibilityText()
+}
