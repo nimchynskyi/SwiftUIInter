abd030b7111b084d8be6e91b046e38ca9988853d
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-12-05 14:41:49 +0100

    update accessibility text view

--- SwiftUIInter/11Accessibility/AccessibilityText.swift
+++ SwiftUIInter/11Accessibility/AccessibilityText.swift
@@ -7,9 +7,12 @@
 
 import SwiftUI
 
-// DynamicText
+// Dynamic Text
 
 struct AccessibilityText: View {
+    
+    @Environment(\.sizeCategory) var sizeCategory
+    
     var body: some View {
         NavigationStack {
             List {
@@ -17,20 +20,38 @@ struct AccessibilityText: View {
                     VStack(alignment: .leading, spacing: 8) {
                         HStack {
                             Image(systemName: "accessibility")
+                                .font(.system(size: 30))
+                            
                             Text("Welcome to this app!")
+                                .truncationMode(.tail)
                         }
                         .font(.title)
                         
                         Text("This is some longer text that expands to multiple lines.")
                             .font(.subheadline)
                             .frame(maxWidth: .infinity, alignment: .leading)
+                            .lineLimit(3)
+                            .minimumScaleFactor(sizeCategory.customMinScaleFactor)
                     }
-                    .frame(height: 100)
+                    //.frame(height: 100)
                     .background(.red)
                 }
             }
             .listStyle(PlainListStyle())
             .navigationTitle("Hello, world!")
+        }
+    }
+}
+
+extension ContentSizeCategory {
+    var customMinScaleFactor: CGFloat {
+        switch self {
+        case .extraSmall, .small, .medium:
+            return 1.0
+        case .large, .extraLarge, .extraExtraLarge:
+            return 0.8
+        default:
+            return 0.6
         }
     }
 }
